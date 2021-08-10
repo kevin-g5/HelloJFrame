1c091462e6ccbab757801fb71356653df22fb46d
Kevin <87554064+kevin-g5@users.noreply.github.com>
2021-08-10 09:39:04 +0100

    Update HelloJava.java
    Added another System.out.println to the Java code.

--- src/com/example/helloworld/HelloJava.java
+++ src/com/example/helloworld/HelloJava.java
@@ -3,5 +3,6 @@ package com.example.helloworld;
 public class HelloJava {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        System.out.println("Was shared with GitHub and edited in browser!");
     }
 }
